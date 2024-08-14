fbe022765971f51e47d3584d0c191d6bce5d25e7
Miwa G <miwa.guhres@hotmail.com>
2024-08-14 12:47:36 +0200

    set login as home

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <SideNav />
       <Routes>
+      <Route path='/' element={<Login/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
 
